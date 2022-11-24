ee4ccdf9e6081196eac7a4950e7e0245d0e6f973
edorguez <edro998@gmail.com>
2022-11-23 23:17:22 -0400

    Fished search alert and added live location when creating alert

---
+++ src/GpsMedicalAssistanceFront/src/app/pages/site-layout/alerts/alerts-create/modal-location-message/modal-location-message.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal-location-message',
+  templateUrl: './modal-location-message.component.html',
+  styleUrls: ['./modal-location-message.component.scss']
+})
+export class ModalLocationMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
